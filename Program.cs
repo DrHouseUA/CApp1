0b85ed47d4187632ba6ab7f0eb99d80fa2f66a95
Роман Княжик <romanknyazhyk@gmail.com>
2021-09-07 12:23:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}!");
+            {
+                Console.WriteLine("First block");
+                {
+                    Console.WriteLine("Second block");
+                }
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
